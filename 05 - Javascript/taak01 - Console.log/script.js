e76463d2b9294e6671c0faefe51f5b44487fce91
Samuel Adu <samuel.adu@student.rocva.nl>
2020-11-02 12:57:41 +0100

    datatypes

--- 05 - Javascript/taak01 - Console.log/script.js
+++ 05 - Javascript/taak01 - Console.log/script.js
@@ -1,1 +1,8 @@
 console.log("Mijn naam is Samuel en dit is mijn eerste console bericht");
+console.log(mijnVoornaam)
+console.log(mijnAchternaam)
+console.log(mijnLeeftijd)
+console.log(isStemgerechtig)
+console.log(isLegaalOmAlcoholTeKopen)
+console.log(ikMagBrommerRijden)
+console.log(softwareOntwikkeling)
