eb6baafcb1cad3234f8f58ace9c9b3015e4d940b
Samuel Adu <samuel.adu@student.rocva.nl>
2020-11-03 17:04:01 +0100

    document object model

--- 05 - Javascript/taak07 - Intelligente DOM/website/script.js
+++ 05 - Javascript/taak07 - Intelligente DOM/website/script.js
@@ -1,9 +1,30 @@
 
-console.log(window.document);
+console.log(document.body.innerHTML);
 
 
 
 // document.querySelector("#posterimg1").src = null;
 
 
-// document.querySelector("#poster1");
+// document.querySelector("#poster1"); 
+var elem = document.querySelector('#poster1');
+
+
+// set a background color
+elem.style.backgroundColor = "purple";
+
+
+// document.querySelector("#poster1"); 
+var elem = document.querySelector('#poster2');
+
+
+// set a background color
+elem.style.backgroundColor = "white"
+
+
+// document.querySelector("#poster1"); 
+var elem = document.querySelector('#poster3');
+
+
+// set a background color
+elem.style.backgroundColor = "blue"
