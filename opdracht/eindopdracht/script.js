98f705d62f3be5b62e2ce7d20d972e916d5b1948
Samuel Adu <samuel.adu@student.rocva.nl>
2020-11-13 10:41:50 +0100

    kleur gefixt

--- opdracht/eindopdracht/script.js
+++ opdracht/eindopdracht/script.js
@@ -8,22 +8,19 @@ console.log (persoon);
 if(persoon.leeftijd==16) {
     document.body.style.backgroundColor = "orange"
     
-} else {
+} else{
     document.body.style.backgroundColor = "purple"
 }
 
     if(persoon.leeftijd==17) {
         document.body.style.backgroundColor = "blue"
-    } else{
-        document.body.style.backgroundColor = "purple"
-    }
+    } 
+    
         if (persoon.leeftijd==18) {
             document.body.style.backgroundColor = "gold"
-        } else {
-            document.body.style.backgroundColor = "purple"
         }
+       
             if (persoon.leeftijd==19) {
                 document.body.style.backgroundColor = "green"
-            } else {
-                document.body.style.backgroundColor = "purple"
             }
+            
